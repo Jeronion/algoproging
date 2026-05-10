d18fb339a12f3f540782c5e27d3d60e9236c01ba
Jeronion <jeronion2010@gmail.com>
2026-05-10 21:07:22 +0300

    _

--- Олимпиада/sol1.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, arr, target):
-        hash_table = {target - v: v for v in arr}
-        for v in arr:
-            if hash_table[v]:
-                return True
-        return False
--- Олимпиада/sol2.py
+++
@@ -1,4 +0,0 @@
-n = int(input())
-X = list(map(int, input().split()))
-T = list(map(int, input().split()))
-dp = [[[0] for _ in range(n)] for _ in range(n)]
